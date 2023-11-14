af9702412744fbbf8fabc4843333df0a1bab35b4
Isaac Wilson <iswilson.dev@gmail.com>
2023-11-14 03:46:16 +0000

    feat: add login page

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,15 +2,18 @@
 import { useRouter } from "vue-router";
 import { ref } from "vue";
 
+const form = ref({
+   email: "",
+   password: "",
+});
 
 const router = useRouter();
 const goToSignup = () => router.push("/signup");
-
 </script>
 
 <template>
-   <main>
-      <div class="grid justify-center items-center">
+   <main class="w-screen">
+      <div class="grid justify-center items-center w-full">
          <div
             class="z-10 flex-col flex text-center justify-center items-center"
          >
@@ -23,17 +26,49 @@ const goToSignup = () => router.push("/signup");
             </p>
          </div>
          <div class="text-center">
-            <button @click="goToSignup"
+            <button
+               @click="goToSignup"
                class="w-48 rounded-lg h-12 text-xl font-semibold hover:bg-secondary-hover bg-secondary-dark text-white transition-colors"
             >
                Sign up
             </button>
          </div>
-         <!-- <div>
-            <form action="POST">
-               <input type="text" />
+         <div class="bg-stone-500 w-100">
+            <form
+               action="/home"
+               method="post"
+               class="flex flex-col justify-center items-center w-full"
+            >
+               <div>
+                  <label for="email">Email</label>
+                  <input
+                     v-model="form.email"
+                     type="email"
+                     name="email"
+                     id="email"
+                     placeholder="name@ug.edu.gh"
+                     class="w-80 h-12 outline-stone-200 outline outline-1 focus:outline-none focus:ring-2 focus:ring-primary-dark focus:border-transparent block rounded-lg"
+                  />
+               </div>
+               <div>
+                  <label for="password">Password</label>
+                  <input
+                     v-model="form.password"
+                     type="password"
+                     name="password"
+                     id="password"
+                     placeholder="password"
+                     class="w-80 h-12 outline-stone-200 outline outline-1 focus:outline-none focus:ring-2 focus:ring-primary-dark focus:border-transparent block rounded-lg"
+                  /><br />
+               </div>
+               <button
+                  type="submit"
+                  class="w-80 rounded-lg h-12 text-xl font-semibold hover:bg-primary-hov bg-primary-dark text-white transition-colors block mx-auto"
+               >
+                  Login
+               </button>
             </form>
-         </div> -->
+         </div>
       </div>
       <div class="absolute">
          <div class="star w-48 h-48 bottom-10"></div>
@@ -48,10 +83,24 @@ const goToSignup = () => router.push("/signup");
    </main>
 </template>
 
-<style>
-body {
+<style scoped>
+main {
    background-color: var(--dark-black);
+   height: 100vh;
 }
+
+label {
+   color: white;
+}
+
+/* input {
+   border-radius: 8px;
+   border: 1px solid white;
+   padding: 5px;
+   margin: 5px;
+
+} */
+
 .star {
    clip-path: polygon(
       50% 0%,
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,9 +6,16 @@ export default {
          colors: {
             dark: "#1F384C",
             "primary-dark": "#5A67BA",
+            "primary-light": "#C7CEFF",
+            "primary-hov": "#6370c5",
             "secondary-dark": "#F99C30",
             "secondary-hover": "#ffae52",
 
+         },
+         spacing: {
+            "100": "28rem",
+            "104": "32rem",
+            "110" : "38rem"
          },
       },
    },
