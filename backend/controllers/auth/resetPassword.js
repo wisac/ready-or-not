09a885a0938b4783b8f089268ffa3a50c52022e5
Isaac Wilson <iswilson.dev@gmail.com>
2024-01-03 00:35:10 +0000

    refactor: format date for password confirmation email

--- backend/controllers/auth/resetPassword.js
+++ backend/controllers/auth/resetPassword.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import crypto from "crypto";
 import CustomError from "../../utils/customError.js";
 import Token from "../../models/tokens.js";
@@ -49,11 +50,32 @@ export default async (req, res, next) => {
    console.log(modifiedUser);
 
    // 6. send response
-   // sendEmail({
-   //    from: "isaac@readyornot.com",
-   //    to: user.email,
-   //    subject: "Password Change Notification",
-   //    text: `This is to confirm that the password for your account was successfully reset. The change was initiated at  on [Insert Date]. If you performed this action, you can disregard this message.`,
-   // });
+   res.status(200).json({
+      status: "success",
+      message:
+         "Your password reset completed successfully. Please sign in with your new password",
+   });
+
    // 7. send email notifying password change
+   const formatPasswordChangeDate = new Date(
+      modifiedUser.passwordChangedAt
+   ).toLocaleDateString("en-us", {
+      month: "long",
+      year: "numeric",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+   });
+   try {
+      sendEmail({
+         from: "isaac@readyornot.com",
+         to: user.email,
+         subject: "Password Reset Confirmation",
+         text: `This is to confirm that the password for your account was successfully reset. The change was initiated on ${formatPasswordChangeDate}. If you performed this action, you can disregard this message.`,
+      });
+   } catch (error) {
+      console.error(
+         `Failed to send password reset confirmation email to ${user.email}`
+      );
+   }
 };
